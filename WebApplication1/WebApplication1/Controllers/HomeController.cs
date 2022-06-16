85fa1c421b6cf330a2eb2ae2bc0242d24ad478dd
agil20 <100156146+agil20@users.noreply.github.com>
2022-06-16 05:24:25 -0700

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
